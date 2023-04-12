645c4d0e89d9a3087c6f18a60fda2dba6b911e4e
DeliaBarron <deliabarron.db@gmail.com>
2023-04-11 21:28:14 -0600

    typescript app, useState() hook implementation

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import List from './components/List'
+import Form from './components/Form'
+//interface
+import { Sub } from './types'
+const INITIAL_STATE = [
+  {
+    nick:'dapelu',
+    subMonths:2,
+    avatar:'https://i.pravatar.cc/150?u=dapelu',
+    description:'moderador lorem'
+  },
+  {
+    nick:'sergio',
+    subMonths:7,
+    avatar:'https://i.pravatar.cc/150?u=sergio_serrano'
+  }
+]
+
+//STATES
+interface AppStates {
+  subs: Sub[] //OR Array<Sub>
+}
+
+function App() {
+  const [subs, setSubs]= useState<AppStates['subs']>([])
+
+  useEffect(()=>{
+    setSubs(INITIAL_STATE)
+  },[])
+  
+  const handleNewSub=(newSub:Sub):void => {
+    setSubs(sub => [...subs, newSub])
+  }
+
+  return (
+    <div className="App">
+     <h1>Title TS + React</h1>
+     <List subs={subs}/>
+     <Form onNewSub={handleNewSub}></Form>
+    </div>
+  )
+}
+
+export default App
